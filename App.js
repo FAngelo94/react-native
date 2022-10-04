b43613fb087b63e67c1045b4625a5bdca12144b0
FAngelo94 <a.falci@live.it>
2022-10-04 21:51:01 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useEffect } from "react";
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View,StatusBar } from 'react-native';
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function App() {
+  /* const webViewRef = useRef(null)
+
+  // Add listener for back button to go back in webview
+  const backAction = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+    }
+  }
+
+    BackHandler.addEventListener("hardwareBackPress", backAction);
+
+ */
+    // const visibility = NavigationBar.useVisibility()
+    NavigationBar.setVisibilityAsync("hidden");
+
+
+    // HIDE ANDROID NAVIGATION BAR
+    useEffect(() => {
+      const hideNavBar = () => {
+        if (Platform.OS === 'android') {
+          StatusBar.setHidden(true);
+          
+        }
+      }
+      hideNavBar();
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        source={{
+          uri: 'https://pwa-offline-243d1.web.app/'
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
   },
 });
---
+++ react-native.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {}
+    },
+    assets: ['./src/assets/'],
+    dependencies: {
+        'react-native-system-navigation-bar': {
+            
+        }
+    }
+
+};
