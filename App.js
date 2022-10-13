f917dff1b73c9d91665704180195e3b501e11293
FAngelo94 <a.falci@live.it>
2022-10-13 21:02:18 +0100

    write over the old application

--- App.js
+++ App.js
@@ -1,42 +1,51 @@
 import React, { useRef, useEffect } from "react";
 import { WebView } from 'react-native-webview';
-import { StyleSheet, View,StatusBar } from 'react-native';
+import { StyleSheet, View, StatusBar, BackHandler, Platform } from 'react-native';
 import * as NavigationBar from 'expo-navigation-bar';
 
 export default function App() {
-  /* const webViewRef = useRef(null)
-
-  // Add listener for back button to go back in webview
-  const backAction = () => {
+  const webViewRef = useRef(null);
+  const onAndroidBackPress = () => {
     if (webViewRef.current) {
       webViewRef.current.goBack();
+      return true; // prevent default behavior (exit app)
     }
-  }
+    return false;
+  };
 
-    BackHandler.addEventListener("hardwareBackPress", backAction);
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+      };
+    }
+  }, []);
 
- */
-    // const visibility = NavigationBar.useVisibility()
-    NavigationBar.setVisibilityAsync("hidden");
+
+  NavigationBar.setVisibilityAsync("hidden");
 
 
-    // HIDE ANDROID NAVIGATION BAR
-    useEffect(() => {
-      const hideNavBar = () => {
-        if (Platform.OS === 'android') {
-          StatusBar.setHidden(true);
-          
-        }
+  // HIDE ANDROID NAVIGATION BAR
+  useEffect(() => {
+    const hideNavBar = () => {
+      if (Platform.OS === 'android') {
+        StatusBar.setHidden(true);
+
       }
-      hideNavBar();
-    }, []);
+    }
+    hideNavBar();
+    // webViewRef.current.clearCache(true);
+    // webViewRef.current.reload();
+  }, []);
 
   return (
     <View style={styles.container}>
       <WebView
         source={{
-          uri: 'https://pwa-offline-243d1.web.app/'
+          uri: 'https://climberworld.web.app/'
         }}
+        ref={webViewRef}
       />
     </View>
   );
--- Readme.md
+++ Readme.md
@@ -1,10 +1,2 @@
-# React Native App
-
-## How run app
-
-To run your project, navigate to the directory and run one of the following npm commands.
-
-- cd ReactNativeApp
-- npm run android
-- npm run ios # you need to use macOS to build the iOS project - use the Expo app if you need to do iOS development without a Mac
-- npm run web
+- npx expo -> execute app
+- eas build -p android --profile preview -> build apk
